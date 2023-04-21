08d2e88ba955ba730ad644bd8d5ccd00ce0aea1d
k3nNyJP <k3nny.51rcy@gmail.com>
2023-04-21 16:30:12 +0900

    [abc194/c] 4/21 training

---
+++ abc194/c/Main.js
@@ -0,0 +1,22 @@
+function Main(input) {
+    const N = parseInt(input[0], 10);
+    const statA = new Map();
+    const A = input[1].split(" ").map((value) => {
+        const parsedValue = parseInt(value, 10);
+        statA.set(parsedValue, (statA.get(parsedValue) || 0) + 1);
+        return parsedValue;
+    });
+    let ans = 0;
+    const existValues = [...statA.keys()];
+    for (let i = 1; i < existValues.length; i++) {
+        for (let j = 0; j < i; j++) {
+            ans +=
+                Math.pow(existValues[i] - existValues[j], 2) *
+                    (statA.get(existValues[i]) || 1) *
+                    (statA.get(existValues[j]) || 1);
+        }
+    }
+    console.log(ans);
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
