e87b12df55f1dd4df21ed8931a84f2be1abfd61e
k3nNyJP <k3nny.51rcy@gmail.com>
2023-06-02 17:05:41 +0900

    [abc299/c] 6/2 training

---
+++ abc299/c/Main.ts
@@ -0,0 +1,28 @@
+function Main(input: string[]) {
+  const N = parseInt(input[0], 10);
+  const S = input[1];
+
+  let maxDangoLevel = -1;
+  let dangoLevel = 0;
+
+  for (let i = 0, foundBar = false; i < N; i++) {
+    if (S[i] === "o") {
+      dangoLevel++;
+      if (!foundBar) {
+        continue;
+      }
+      maxDangoLevel = Math.max(maxDangoLevel, dangoLevel);
+    } else {
+      if (!foundBar && dangoLevel > 0) {
+        maxDangoLevel = Math.max(maxDangoLevel, dangoLevel);
+      }
+      dangoLevel = 0;
+      foundBar = true;
+    }
+  }
+
+  console.log(maxDangoLevel);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
