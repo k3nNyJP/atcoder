d40ad24e1e51b5b89373d709630370516b3aa209
k3nNyJP <k3nny.51rcy@gmail.com>
2023-08-25 16:09:23 +0900

    [abc314/c] 8/18 training

---
+++ abc314/c/Main.js
@@ -0,0 +1,18 @@
+function Main(input) {
+    const [N, M] = input[0].split(" ").map((value) => parseInt(value, 10));
+    const S = input[1];
+    const C = input[2].split(" ").map((value) => parseInt(value, 10));
+    const parsedS = Array(M).fill("");
+    for (let i = 0; i < N; i++) {
+        parsedS[C[i] - 1] += S[i];
+    }
+    const rotatedS = parsedS.map((str) => str[str.length - 1] + str.substring(0, str.length - 1));
+    let ans = "";
+    for (let i = 0; i < N; i++) {
+        ans += rotatedS[C[i] - 1][0];
+        rotatedS[C[i] - 1] = rotatedS[C[i] - 1].substring(1);
+    }
+    console.log(ans);
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
