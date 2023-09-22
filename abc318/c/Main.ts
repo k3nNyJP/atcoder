2dd0fe5a0fc9e8f34e6fd1e3deb85944f2c68aff
k3nNyJP <k3nny.51rcy@gmail.com>
2023-09-22 16:23:00 +0900

    [abc318/c] 9/22 training

---
+++ abc318/c/Main.ts
@@ -0,0 +1,24 @@
+function Main(input: string[]) {
+  const [N, D, P] = input[0].split(" ").map((value) => parseInt(value, 10));
+  const fees = input[1]
+    .split(" ")
+    .map((value) => parseInt(value, 10))
+    .sort((a, b) => b - a);
+
+  let ans = 0;
+
+  for (let i = 0; i < N; i += D) {
+    let sumOfFeesInDDays = 0;
+
+    for (let day = 0; day < D && i + day < N; day++) {
+      sumOfFeesInDDays += fees[i + day];
+    }
+
+    ans += P < sumOfFeesInDDays ? P : sumOfFeesInDDays;
+  }
+
+  console.log(ans);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
