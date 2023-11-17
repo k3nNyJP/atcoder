8b327f748314017eaa94301621cac2256e6dffea
k3nNyJP <k3nny.51rcy@gmail.com>
2023-11-17 17:52:42 +0900

    [abc326/c] 11/17 training

---
+++ abc326/c/Main.ts
@@ -0,0 +1,25 @@
+function Main(input: string[]) {
+  const [N, M] = input[0].split(" ").map((value) => parseInt(value, 10));
+  const A = input[1]
+    .split(" ")
+    .map((value) => parseInt(value, 10))
+    .sort((a, b) => a - b);
+
+  let ans = 0;
+
+  for (let head = 0, tail = 0; head < N && tail < N; head++) {
+    while (A[tail + 1] - A[head] < M) {
+      tail++;
+      if (tail >= N) {
+        break;
+      }
+    }
+
+    ans = tail + 1 - head > ans ? tail + 1 - head : ans;
+  }
+
+  console.log(ans);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
