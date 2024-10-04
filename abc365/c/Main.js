42b7d656140ad8994bdfa990276dcac45081201a
k3nNyJP <k3nny.51rcy@gmail.com>
2024-10-04 17:55:56 +0900

    [abc365/c] 10/04 training

---
+++ abc365/c/Main.js
@@ -0,0 +1,32 @@
+function Main(input) {
+    const [N, M] = input[0].split(" ").map((value) => parseInt(value, 10));
+    const A = input[1]
+        .split(" ")
+        .map((value) => parseInt(value, 10))
+        .sort((a, b) => a - b);
+    const totalA = A.reduce((sum, value) => sum + value);
+    if (totalA <= M) {
+        console.log("infinite");
+        return;
+    }
+    let shortage = totalA - M;
+    let allowance = Math.floor(M / N);
+    while (true) {
+        let surplus = 0;
+        for (let i = 0; A[i] < allowance; i++) {
+            surplus += allowance - A[i];
+        }
+        const payment = allowance * N - surplus;
+        if (payment > M || M - payment > N) {
+            // まだ不足 or 余りすぎ
+            allowance -= Math.floor((payment - M) / N);
+        }
+        else {
+            // 適正
+            console.log(allowance);
+            return;
+        }
+    }
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
