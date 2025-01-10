26696b37801019a10d7d72ca24786eb64b78b08d
k3nNyJP <k3nny.51rcy@gmail.com>
2025-01-10 17:07:00 +0900

    [abc380/c] 1/10 training

---
+++ abc380/c/Main.ts
@@ -0,0 +1,31 @@
+function Main(input: string[]) {
+  const [N, K] = input[0].split(" ").map((value) => parseInt(value, 10));
+  const S = input[1];
+
+  const blockRegExp = /1+/g;
+
+  let block: RegExpExecArray | null;
+  const foundBlocks: RegExpExecArray[] = [];
+
+  while ((block = blockRegExp.exec(S)) !== null) {
+    foundBlocks.push(block);
+
+    if (foundBlocks.length >= K) {
+      break;
+    }
+  }
+
+  const prevBlock = foundBlocks[K - 2];
+  const moveBlock = foundBlocks[K - 1];
+
+  const answer =
+    S.slice(0, prevBlock.index + prevBlock[0].length) +
+    moveBlock[0] +
+    S.slice(prevBlock.index + prevBlock[0].length, moveBlock.index) +
+    S.slice(moveBlock.index + moveBlock[0].length);
+
+  console.log(answer);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
